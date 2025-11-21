f6f8a6f9e7462fa3e13a22439d815ef18f3c1ea0
Kalyan <np01ai4a250016@islingtoncollege.edu.np>
2025-11-21 12:21:40 +0545

    Initial sharing of project

---
+++ edxample.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class edxample here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class edxample
+{
+    
+    public static void main (string[]arg)
+    {
+       
+    }
+    
+    {
+
+    }
+}
